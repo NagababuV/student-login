005cf9dcb622b4916602c81e0ef4489fd01a12dd
NagababuV <nagababuvegisetti1313@gmail.com>
2024-07-16 11:04:58 +0530

    initial commit

---
+++ src/main/java/com/login/student/model/User.java
@@ -0,0 +1,21 @@
+package com.login.student.model;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+
+    private String id;
+    private String username;
+    private String password;
+    private List<String> roles;
+}
